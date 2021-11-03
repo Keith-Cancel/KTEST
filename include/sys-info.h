e53ed42a426304a3e3c4b1a72638633ee12cc053
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2021-11-02 17:36:56 -0700

    Helper functions to get information about the system

---
+++ include/sys-info.h
@@ -0,0 +1,40 @@
+#ifndef KSYS_INFO_H
+#define KSYS_INFO_H
+
+#include <stdint.h>
+
+#define ARCH_UNKNOWN  0x0000U
+#define X86_32        0x0100U
+#define X86_64        0x0101U
+#define ARM_64        0x0200U
+
+#if defined(__x86_64__) || defined(_M_X64)
+    #define CURRENT_ARCH X86_64
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    #define CURRENT_ARCH X86_32
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    #define CURRENT_ARCH ARM_64
+#else
+    #define CURRENT_ARCH ARCH_UNKNOWN
+#endif
+
+#define OS_UNKNOWN   0x0000U
+#define OS_LINUX     0x0100U
+#define OS_WINDOWS   0x0200U
+#define OS_UNIX_LIKE 0x0F00U
+
+#if defined(_WIN32) || defined(__WIN32)
+    #define CURRENT_OS OS_WINDOWS
+#elif defined(__linux__)
+    #define CURRENT_OS OS_LINUX
+#elif defined(__unix__)
+    #define CURRENT_OS OS_UNIX_LIKE
+#else
+    #define CURRENT_OS OS_UNKNOWN
+#endif
+
+unsigned get_arch();
+unsigned get_os();
+size_t   get_memory_page_size();
+
+#endif
---
+++ src/sys-info.c
@@ -0,0 +1,36 @@
+#include "sys-info.h"
+
+unsigned get_arch() {
+    return CURRENT_ARCH;
+}
+
+unsigned get_os() {
+    return CURRENT_OS;
+}
+
+#if CURRENT_OS == OS_WINDOWS
+#include <windows.h>
+
+size_t get_memory_page_size() {
+    SYSTEM_INFO info;
+    GetSystemInfo(&info);
+    return info.dwPageSize;
+}
+
+#elif CURRENT_OS == OS_LINUX
+#include <unistd.h>
+
+size_t get_memory_page_size() {
+    return getpagesize();
+}
+
+#elif CURRENT_OS == OS_UNIX_LIKE
+#include <unistd.h>
+
+size_t get_memory_page_size() {
+    return sysconf(_SC_PAGESIZE);
+}
+
+#else
+    #error "Not ported to this OS"
+#endif
